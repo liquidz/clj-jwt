0360e9f950902bcea67abf95e892af7457542fd1
liquidz <liquidz.uo@gmail.com>
2014-08-11 07:40:28 +0900

    update dependencies

--- project.clj
+++ project.clj
@@ -4,10 +4,10 @@
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.2.4"]
+                 [org.clojure/data.json "0.2.5"]
                  [org.clojure/data.codec "0.1.0"]
                  [org.bouncycastle/bcprov-jdk15 "1.46"]
-                 [clj-time "0.7.0"]]
+                 [clj-time "0.8.0"]]
 
   :profiles {:dev {:dependencies [[midje "1.6.3"  :exclusions [org.clojure/clojure]]]}}
   :plugins  [[lein-midje "3.1.3"]])
