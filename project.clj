f0cf0d19b3ee24960e8d055554f503c347a1f05d
liquidz <liquidz.uo@gmail.com>
2015-04-02 07:40:58 +0900

    see #13 update data.json dependency

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject clj-jwt "0.0.12"
+(defproject clj-jwt "0.0.13"
   :description  "Clojure library for JSON Web Token(JWT)"
   :url          "https://github.com/liquidz/clj-jwt"
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/data.json "0.2.5"]
+                 [org.clojure/data.json "0.2.6"]
                  [org.clojure/data.codec "0.1.0"]
                  [org.bouncycastle/bcprov-jdk15 "1.46"]
                  [crypto-equality "1.0.0"]
